ba081bc724c78ef71ce6ba5719428558bbddf672
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-09-04 17:38:20 -0500

    chore: rename app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'me.techandgar.demo.nequi.todo',
-  appName: 'Nequi TO-DO',
+  appName: 'TO-DO',
   webDir: 'www'
 };
 
