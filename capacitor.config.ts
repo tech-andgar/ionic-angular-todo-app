de34e8d89ecf8e631499eaa3b3c6387d04013699
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-09-04 16:48:27 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Nequi TO-DO',
+  webDir: 'www'
+};
+
+export default config;
