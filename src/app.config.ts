3cd5c48ddd171d1221775205cf0e5bdd575feb12
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-09-08 21:19:11 -0500

    fix: bug compilation

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,22 +3,18 @@ import { CategoriesApi } from './app/features/categories/domain/infrastructure/c
 import { CategoriesRepository } from './app/features/categories/domain/repository/categories_repository';
 import { CategoriesRepositoryImpl } from './app/features/categories/data/repository/categories-repository-impl';
 import { DataService } from './app/services/data.service';
-import { environment } from './environments/environment';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { LocalStorageCategoriesApi } from './app/features/categories/data/infrastructure/local-storage-api/local.storage.categories.api';
-import { LocalStorageTodosApi } from './app/features/todos/data/infrastructure/local_storage_api/local.storage.todos.api';
+import { LocalStorageTodosApi } from './app/features/todos/data/infrastructure/local_storage/local.storage.todos.api';
+import { Observable } from 'rxjs';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { routes } from './app/app.routes';
 import { TodosApi } from './app/features/todos/domain/infrastructure/todos_api';
 import { TodosRepository } from './app/features/todos/domain/repository/todos_repository';
 import { TodosRepositoryImpl } from './app/features/todos/data/repository/todos-repository-impl';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
-import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 export class LazyTranslateLoader implements TranslateLoader {
   constructor(private http: HttpClient) {}
@@ -46,8 +42,6 @@ export const appConfig: ApplicationConfig = {
     provideIonicAngular(),
     provideHttpClient(),
     importProvidersFrom(IonicStorageModule.forRoot()),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
     importProvidersFrom(
       TranslateModule.forRoot({
         loader: {
